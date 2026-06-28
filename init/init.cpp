a80984af9c8936a20a177153c4686f8f3ece9152
Duke Brahma <dukebraham24@gmail.com>
2026-06-28 21:27:04 +0530

    even: init: fix the market name set wrong for rmx3197

--- init/init.cpp
+++ init/init.cpp
@@ -81,7 +81,7 @@ void vendor_load_properties() {
         property_override("persist.sys.axion_cpu_info", "MTK_Helio_G85");
         property_override("persist.sys.device_camera_info_rear", "48,2,2");
     } else if (prjname == "2167D") {
-        set_device_props("RMX3197", "Realme C25");
+        set_device_props("RMX3197", "Realme C25S");
         property_override("persist.sys.axion_cpu_info", "MTK_Helio_G85");
         property_override("persist.sys.device_camera_info_rear", "13,2,2");
     } else if (prjname == "216AF") {
