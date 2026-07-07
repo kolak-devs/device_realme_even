d417357f243d9b8ac4284a8cb42616f3ece2f808
kolak-devs <rulsrender2@gmail.com>
2026-07-08 02:53:45 +0700

    even: vendorsetup: change repo
    Signed-off-by: kolak-devs <rulsrender2@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo start cloning repos
 VT=vendor/realme/even/even-vendor.mk
-if ! [ -a $VT ]; then git clone -b main-oss https://github.com/Badmaneers/vendor_realme_even_rui4 --depth=1 vendor/realme/even
+if ! [ -a $VT ]; then rm -rf vendor/realme/even && git clone -b main-oss https://github.com/kolak-devs/vendor_realme_even_rui4.git --depth=1 vendor/realme/even
 fi
 KT=kernel/realme/even/KernelSU/kernel/Kconfig
 if ! [ -a $KT ]; then rm -rf kernel/realme/even && git clone --recurse-submodules https://github.com/Badmaneers/zenium_realme_even --depth=1 kernel/realme/even
@@ -15,7 +15,7 @@ OPLUS=hardware/oplus/Android.bp
 if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
 fi
 VT-IMS=vendor/mediatek/ims/ims.mk
-if ! [ -a $VT-IMS ]; then git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+if ! [ -a $VT-IMS ]; then rm -rf vendor/mediatek/ims && git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 fi
 POCKET=packages/apps/PocketMode/pocket_mode.mk
 if ! [ -a $POCKET ]; then git clone https://github.com/nishant6342/packages_apps_PocketMode packages/apps/PocketMode
