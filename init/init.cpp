0a812bf0dfc3cd8e163bd7c522aa5640a3264a85
kolak-devs <rulsrender2@gmail.com>
2026-07-12 07:59:48 +0700

    even: misc

--- init/init.cpp
+++ init/init.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The LineageOS Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,67 +14,68 @@
  * limitations under the License.
  */
 
-#include <string>
-#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <sys/sysinfo.h>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
+#include "property_service.h"
 
-using android::base::GetProperty;
+void property_override(char const prop[], char const value[])
+{
+    auto pi = (prop_info *) __system_property_find(prop);
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "system.",
-    "product.",
-    "system_ext.",
-    "vendor.",
-    "vendor_dlkm.",
-};
-
-void property_override(char const prop[], char const value[]) {
-    prop_info *pi;
-
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi)
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else
+    } else {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
 
-void set_device_props(const std::string model, const std::string marketname) {
-    const auto set_ro_product_prop = [](const std::string &source,
-                                        const std::string &prop,
-                                        const std::string &value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str());
-    };
+void load_dalvik_properties() {
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+    struct sysinfo sys;
 
-    for (const auto &source : ro_props_default_source_order) {
-        set_ro_product_prop(source, "device", model);
-        set_ro_product_prop(source, "model", model);
-        set_ro_product_prop(source, "name", model);
-        set_ro_product_prop(source, "marketname", marketname);
+    sysinfo(&sys);
+
+    if (sys.totalram >= 5ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram >= 3ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        return;
     }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
 void vendor_load_properties() {
-    std::string prjname = GetProperty("ro.boot.prjname", "");
-
-    if (prjname == "20761") {
-        set_device_props("RMX3191", "Realme C25");
-    } else if (prjname == "20762") {
-        set_device_props("RMX3193", "Realme C25");
-    } else if (prjname == "2167A") {
-        set_device_props("RMX3195", "Realme C25S");
-    } else if (prjname == "2167C") {
-        set_device_props("RMX3195", "Realme C25S");
-    } else if (prjname == "2167D") {
-        set_device_props("RMX3197", "Realme C25S");
-    } else if (prjname == "216AF") {
-        set_device_props("RMX3430", "Realme Narzo 50A");
-    }
+    load_dalvik_properties();
 }
